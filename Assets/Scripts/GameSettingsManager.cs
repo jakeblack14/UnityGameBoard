5b68dbacb2d158406e64ec41f63ff8e3036af5c8
Anna <acrowder1@harding.edu>
2018-02-04 21:30:24 -0800

    small changes to the game settings menu

--- Assets/Scripts/GameSettingsManager.cs
+++ Assets/Scripts/GameSettingsManager.cs
@@ -5,24 +5,31 @@ using UnityEngine.UI;
 
 public class GameSettingsManager : MonoBehaviour {
 
+    private LoadSceneOnClick loadScene = new LoadSceneOnClick();
+
     public List<Button> locationButtons;
     public List<Button> levelButtons;
     public List<Button> turnButtons;
 
     private Color32 selectedColor = new Color32(230, 101, 255, 255);
 
-    private Button currentButton;
+    private Button currentLocationButton;
+    private Button currentLevelButton;
+    private Button currentTurnButton;
+
+    private string currentLevelSelected;
+    private string currentTurnSelected;
 
     public void locationSelected(int index)
     {
-        currentButton = locationButtons[index];
+        currentLocationButton = locationButtons[index];
 
-        ColorBlock tempColorBlock = currentButton.colors;
+        ColorBlock tempColorBlock = currentLocationButton.colors;
         ColorBlock originalColorBlock = tempColorBlock;
 
         tempColorBlock.normalColor = selectedColor;
         tempColorBlock.highlightedColor = selectedColor;
-        currentButton.colors = tempColorBlock;
+        currentLocationButton.colors = tempColorBlock;
 
         for (int i = 0; i < 2; i++)
         {
@@ -35,14 +42,14 @@ public class GameSettingsManager : MonoBehaviour {
 
     public void levelSelected(int index)
     {
-        currentButton = levelButtons[index];
+        currentLevelButton = levelButtons[index];
 
-        ColorBlock tempColorBlock = currentButton.colors;
+        ColorBlock tempColorBlock = currentLevelButton.colors;
         ColorBlock originalColorBlock = tempColorBlock;
 
         tempColorBlock.normalColor = selectedColor;
         tempColorBlock.highlightedColor = selectedColor;
-        currentButton.colors = tempColorBlock;
+        currentLevelButton.colors = tempColorBlock;
 
         for (int i = 0; i < 3; i++)
         {
@@ -55,14 +62,14 @@ public class GameSettingsManager : MonoBehaviour {
 
     public void turnSelected(int index)
     {
-        currentButton = turnButtons[index];
+        currentTurnButton = turnButtons[index];
 
-        ColorBlock tempColorBlock = currentButton.colors;
+        ColorBlock tempColorBlock = currentTurnButton.colors;
         ColorBlock originalColorBlock = tempColorBlock;
 
         tempColorBlock.normalColor = selectedColor;
         tempColorBlock.highlightedColor = selectedColor;
-        currentButton.colors = tempColorBlock;
+        currentTurnButton.colors = tempColorBlock;
 
         for (int i = 0; i < 2; i++)
         {
@@ -70,6 +77,25 @@ public class GameSettingsManager : MonoBehaviour {
             {
                 turnButtons[i].colors = originalColorBlock;
             }
+        }
+    }
+
+    public void nextButtonClicked()
+    {
+        //if(currentLevelButton == levelButtons[0])
+        //{
+        //    currentLevelSelected = "Easy";
+        //}
+
+        //Debug.Log(currentLevelSelected);
+
+        if (currentLocationButton == locationButtons[0])
+        {
+            loadScene.LoadByIndex(2);
+        }
+        else
+        {
+            loadScene.LoadByIndex(1);
         }
     }
 }
