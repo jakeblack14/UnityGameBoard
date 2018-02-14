a063291d22ddd25c4074b5303a53b5ffec516ea9
Lab <ablack3@harding.edu>
2018-02-14 15:04:39 -0600

    Minor Changes

---
+++ Assets/Scripts/GameTutorialDisappear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTutorialDisappear : MonoBehaviour
+{
+    GameObject Tutorial;
+
+    static bool on = false;
+    // Use this for initialization
+
+    public void CheckHelpMenu()
+    {
+        if (on)
+        {
+            Tutorial.SetActive(false);
+            on = false;
+        }
+        else
+        {
+            Tutorial.SetActive(true);
+            on = true;
+        }
+    }
+}
+
