7156248b576567d6f02692a045f7a66a55d1649e
Lab <ablack3@harding.edu>
2018-02-15 12:13:40 -0600

    Did some work on the Help Menu
    Everything is now fuinctioning the way I want it to. Now is just a matter of making it actually look decent and have the material that we will need for it.
    
    Debating actually making a small series of videos for the help menu... But that is something to be considered with the group later today

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -6,46 +6,64 @@ using UnityEngine.UI;
 public class Tutorial : MonoBehaviour {
 
     public Sprite[] s1;
-    public GameObject BackButton;
-    public GameObject NextButton;
     public Image I1;
+    public Image Next;
+    public Image Back;
+    public string[] ScreenText;
+    public Text TextBox;
 
     static int count = 0;
     // Drag your second sprite here
 
     void Start()
     {
-        if(count == 0)
+        if (count == 0)
         {
-            BackButton.SetActive(false);
+            Back.enabled = false;
         }
+
+        ScreenText = new string[5];
+        ScreenText[0] = "Whether the scrollbar should automatically be hidden when it isn’t needed, and optionally expand the viewport as well.";
+        ScreenText[1] = "TESTIONG ALL THEXSTFADKJFHSDKJF KJHF sdkfjh KJFHsk kjsdf";
+        ScreenText[2] = "STILL TESTING THE MESS OUT OF TEXT.";
+        ScreenText[3] = "PUTTING THE MESS INTO THIS TEXT";
+        ScreenText[4] = "PUTTING THE MESS INTO THIS TEXT";
         s1 = Resources.LoadAll<Sprite>("Sprites");
-        
+
+        TextBox.text = ScreenText[0];
+
+        I1.sprite = s1[count];
     }
 
     public void On_Click_Button()
     {
-       
+        if (count != s1.Length - 1)
+        {
             count++;
             I1.sprite = s1[count];
-            BackButton.SetActive(true);
+            TextBox.text = ScreenText[count];
+            Back.enabled = true;
+        }
         
 
         if(count == s1.Length - 1)
         {
-            NextButton.SetActive(false);
+            Next.enabled = false;
         }
     }
     public void On_Back_Click_Button()
     {
-      
+        if (count != 0)
+        {
             count--;
             I1.sprite = s1[count];
-            NextButton.SetActive(true);
+            TextBox.text = ScreenText[count];
+            Next.enabled = true;
+        }
 
         if (count == 0)
         {
-            BackButton.SetActive(false);
+            Back.enabled = false;
         }
             
     } 
