9f80c9c7a35a0373daf6b711ad798c4a1cb6c65e
Anna <acrowder1@harding.edu>
2018-02-21 18:41:37 -0600

    You can now pull up the tutorial menu in the gameboard scene. Also I added some functionality to the settings menu

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+    public InputField username;
+    public Image character;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void Update()
+    {
+        username.text = GameBoardData.Name;
+
+        if (GameBoardData.IsAlien)
+        {
+            character.sprite = GameBoardData.Alien;
+        }
+        else
+        {
+            character.sprite = GameBoardData.Astronaut;
+        }
+    }
+}
