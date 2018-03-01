6654aca027d7b089237b6dad45860e70ed75c1fc
Lab <ablack3@harding.edu>
2018-03-01 17:01:49 -0600

    Made changes to the Milky Way scene. The board is now semitransparent and there is more movement in the background

--- Assets/Scripts/ObjectSpin.cs
+++ Assets/Scripts/ObjectSpin.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ObjectSpin : MonoBehaviour {
 
     // Use this for initialization
-    int spinx = -1;
-    int spiny = 1;
-    int spinz = 1;
+    //float spinx = -0.1f;
+    //float spiny = 0.1f;
+    //float spinz = 0.1f;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(spinx, spiny, spinz);
+        //transform.Rotate(spinx, spiny, spinz);
+
+        transform.Rotate(Vector3.up * 0.06f, Space.Self);
 	}
 }
