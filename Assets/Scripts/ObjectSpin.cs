b309182ddcc9ec25e9e9e6a321e2ec1b8c499049
Anna <acrowder1@harding.edu>
2018-03-14 18:01:44 -0500

    Finished the Main Menu scene animations

--- Assets/Scripts/ObjectSpin.cs
+++ Assets/Scripts/ObjectSpin.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpin : MonoBehaviour {
-	
+
+    public float spin = 0.1f;
+
 	// Update is called once per frame
 	void Update () {
 
-        transform.Rotate(Vector3.up * 0.1f, Space.Self);
+        transform.Rotate(Vector3.up * spin, Space.Self);
 	}
 }
