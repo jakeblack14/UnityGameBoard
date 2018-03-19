8997a8d8b64f3a9ccd740e7a1324a646485df8ce
Lab <ablack3@harding.edu>
2018-03-19 15:08:14 -0500

    Music with Button to main menu

---
+++ Assets/Scripts/MusicChanges.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicChanges : MonoBehaviour {
+
+
+    AudioSource Music;
+    public Button MyButton;
+    
+	// Use this for initialization
+	void Start () {
+        Button btn = MyButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        Music = GetComponent<AudioSource>();
+    }
+
+    void TaskOnClick()
+    {
+        if(Music.mute == false)
+        {
+            Music.mute = true;
+        }
+        else
+        {
+            Music.mute = false;
+        }
+    }
+}
