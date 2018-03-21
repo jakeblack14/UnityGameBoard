89466764ebdea74c34663250037f87627c6e998a
Lab <ablack3@harding.edu>
2018-03-21 17:02:55 -0500

    Implemented color blind mode in the main menu scene

---
+++ Assets/Scripts/ColorBlindMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class ColorBlindMode : MonoBehaviour {
+
+    public PostProcessingProfile colorBlindModeProfile;
+    private bool inColorBlindMode = false;
+
+    public void SetHue()
+    {
+        var HueShiftValue = colorBlindModeProfile.colorGrading.settings;
+
+        if(inColorBlindMode)
+        {
+            HueShiftValue.basic.hueShift = 0;
+            colorBlindModeProfile.colorGrading.settings = HueShiftValue;
+            inColorBlindMode = false;
+        }
+        else
+        {
+            HueShiftValue.basic.hueShift = -89;
+            colorBlindModeProfile.colorGrading.settings = HueShiftValue;
+            inColorBlindMode = true;
+        }
+    }
+}
