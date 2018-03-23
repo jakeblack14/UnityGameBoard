ecf3ba25bbe499f86c455c92d2744ac8200616f5
Lab <ablack3@harding.edu>
2018-03-23 18:17:39 -0500

    Trying different things with the loading panel. Also the asteroid shadows are now lighter

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public Image Rocket;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rocket.transform.position += (Vector3.right * speed) * Time.deltaTime;
+	}
+}
